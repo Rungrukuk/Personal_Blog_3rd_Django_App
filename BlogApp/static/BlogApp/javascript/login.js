0814d9717233aed8a904d0fa874c77ff1ad5e6ce
Kamal Azizov <70916892+Rungrukuk@users.noreply.github.com>
2023-08-21 18:53:37 +0400

    Register page modified

--- BlogApp/static/BlogApp/javascript/login.js
+++
@@ -1,7 +0,0 @@
-if(document.getElementById('register-container')){
-    document.getElementById('register-container').style.height = '450px';
-}
-if(document.getElementById('login-container')){
-    document.getElementById('login-container').style.height = '350px';
-}
-
