ec72302f9c417cab12832f3d2dc8b87fba6950cf
Kamal Azizov <70916892+Rungrukuk@users.noreply.github.com>
2023-09-03 23:53:15 +0400

    Added searchbar to front end

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     username = models.CharField(max_length=30, unique=True, null=False, blank=False,default="example_username123")
     password = models.CharField(max_length=128, null=False, blank=False,default="example_password123")
     profile_picture_path = models.CharField(max_length=255, null=True, blank=True, default="/media/profile_images/default.jpg")
-
+    friends = models.ManyToManyField('self', blank=True)
 
 class BlogPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
