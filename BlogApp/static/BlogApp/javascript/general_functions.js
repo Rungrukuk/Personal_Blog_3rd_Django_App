8c6c8d550cf17edbf692f01202e1a8f270de03fd
Kamal Azizov <70916892+Rungrukuk@users.noreply.github.com>
2023-10-01 20:09:11 +0400

    Improved Friend button added actions box

--- BlogApp/static/BlogApp/javascript/general_functions.js
+++ BlogApp/static/BlogApp/javascript/general_functions.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Hide all friend-actions when clicking outside any friend and show the friend-actions of the clicked friend
     document.addEventListener("click", function (e) {
-        const allFriendActions = document.querySelectorAll(".friend-actions");
-        allFriendActions.forEach(fa => fa.style.display = 'none');
         
-        let friend = e.target.closest('.friend');
-        if (friend) {
-            const friendActions = friend.nextElementSibling;
-            friendActions.style.display = 'block';
-
-            const rect = friend.getBoundingClientRect();
-            friendActions.style.left = (rect.right + 10) + 'px'; 
-            friendActions.style.top = rect.top + 'px'; 
+        let clickedFriend = e.target.closest('.friend');
+        
+        const allFriendActions = document.querySelectorAll(".friend-actions");
+        
+        allFriendActions.forEach(fa => {
+            // If the clicked friend's actions are already visible, hide them
+            if(clickedFriend && fa === clickedFriend.nextElementSibling && fa.style.display === 'flex') {
+                fa.style.display = 'none';
+                clickedFriend = null; // Set to null so it doesn’t show the friend-actions in the next steps
+            } else {
+                fa.style.display = 'none';
+            }
+        });
+        
+        if (clickedFriend) {
+            const friendActions = clickedFriend.nextElementSibling;
+            friendActions.style.display = 'flex';
+            
+            const rect = clickedFriend.getBoundingClientRect();
+            friendActions.style.left = (rect.right + 10) + 'px';
+            friendActions.style.top = rect.top + 'px';
         }
     });
 });
